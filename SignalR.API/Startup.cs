fc7888c49c8cfbcb1e20e6f3b8bf0c560fc68037
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-01 14:13:42 +0300

    Some library added

--- SignalR.API/Startup.cs
+++ SignalR.API/Startup.cs
@@ -27,12 +27,12 @@ namespace SignalR.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+         
+
+      
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SignalR.API", Version = "v1" });
-            });
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,19 +41,21 @@ namespace SignalR.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SignalR.API v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                ///http://localhost:4400/MyHub
                 endpoints.MapHub<MyHub>("/MyHub");
             });
         }
